53dfd6c5fe1a44d6c04cdd9d0df016c1fca97340
Ravindra Sisodia <22148496+RSCipher001@users.noreply.github.com>
2021-06-10 22:43:12 +0530

    More refactor

--- app/Services/ProjectService.ts
+++ app/Services/ProjectService.ts
@@ -1,15 +1,17 @@
 import Application from '@ioc:Adonis/Core/Application'
 import { string } from '@ioc:Adonis/Core/Helpers'
 import ProjectInput, { Table } from 'App/Interfaces/ProjectInput'
-import APIInit from 'App/Services/Backend/Init'
-import DatabaseGenerator from 'App/Services/Backend/DatabaseGenerator'
-import BackendAuthGenerator from 'App/Services/Backend/AuthGenerator'
-import FrontendAuthGenerator from 'App/Services/Frontend/AuthGenerator'
 import HelperService from 'App/Services/HelperService'
-import BackendTestGenerator from './Backend/TestGenerator'
-import BackendCRUDGenerator from './Backend/CRUDGenerator'
-import FrontendCRUDGenerator from './Frontend/CRUDGenerator'
-import SPAInit from './Frontend/Init'
+
+import AdonisInit from 'App/Services/Backend/Adonis/Init'
+import AdonisDatabaseGenerator from 'App/Services/Backend/Adonis/DatabaseGenerator'
+import AdonisAuthGenerator from 'App/Services/Backend/Adonis/AuthGenerator'
+import AdonisCRUDGenerator from 'App/Services/Backend/Adonis/CRUDGenerator'
+import AdonisTestGenerator from 'App/Services/Backend/Adonis/TestGenerator'
+
+import BuefyInit from 'App/Services//Frontend/Buefy/Init'
+import BuefyAuthGenerator from 'App/Services/Frontend/Buefy/AuthGenerator'
+import BuefyCRUDGenerator from 'App/Services/Frontend/Buefy/CRUDGenerator'
 
 class BackendProjectService {
   private input: any
@@ -44,7 +46,7 @@ class BackendProjectService {
   public prepare(): ProjectInput {
     const projectInput: any = {}
     projectInput.camelCaseStrategy = !!this.input.camelCaseStrategy
-    projectInput.skip = this.input.skip
+    projectInput.generate = this.input.generate
     projectInput.names = HelperService.generateExtendedNames(this.input.name)
     projectInput.name = projectInput.names.pascalCase
     projectInput.projectsPath = Application.makePath('../projects')
@@ -74,51 +76,51 @@ class BackendProjectService {
   public async start() {
     try {
       // Generate Project
-      if (!this.input.skip.init) {
-        const init = new APIInit(this.projectInput)
+      if (this.projectInput.generate.api.generate) {
+        const init = new AdonisInit(this.projectInput)
         await init.init() // Initialize project
-      }
 
-      // Add database
-      if (!this.input.skip.db) {
-        const db = new DatabaseGenerator(this.projectInput)
-        await db.init()
-      }
+        // Add database
+        if (this.projectInput.generate.api.db) {
+          const db = new AdonisDatabaseGenerator(this.projectInput)
+          await db.init()
+        }
 
-      // Add Auth
-      if (!this.input.skip.auth) {
-        const auth = new BackendAuthGenerator(this.projectInput)
-        await auth.init()
-      }
+        // Add Auth
+        if (this.projectInput.generate.api.db && this.projectInput.generate.api.auth) {
+          const auth = new AdonisAuthGenerator(this.projectInput)
+          await auth.init()
+        }
 
-      // Add CRUD for models
-      if (!this.input.skip.crud) {
-        const crud = new BackendCRUDGenerator(this.projectInput)
-        await crud.init()
-      }
+        // Add CRUD for models
+        if (this.projectInput.generate.api.db && this.projectInput.generate.api.crud) {
+          const crud = new AdonisCRUDGenerator(this.projectInput)
+          await crud.init()
+        }
 
-      // Add Tests for everything
-      if (!this.input.skip.test) {
-        const test = new BackendTestGenerator(this.projectInput)
-        await test.init()
+        // Add Tests for everything
+        if (this.projectInput.generate.api.auth && this.projectInput.generate.api.test) {
+          const test = new AdonisTestGenerator(this.projectInput)
+          await test.init()
+        }
       }
 
       // Prepare frontend
-      if (!this.input.skip.spa) {
-        const spa = new SPAInit(this.projectInput)
+      if (this.projectInput.generate.spa.generate) {
+        const spa = new BuefyInit(this.projectInput)
         await spa.init()
-      }
 
-      // Prepare frontend auth
-      if (!this.input.skip.spaAuth) {
-        const auth = new FrontendAuthGenerator(this.projectInput)
-        await auth.init()
-      }
+        // Prepare frontend auth
+        if (this.projectInput.generate.spa.auth) {
+          const auth = new BuefyAuthGenerator(this.projectInput)
+          await auth.init()
+        }
 
-      // Add CRUD for models
-      if (!this.input.skip.spaCrud) {
-        const crud = new FrontendCRUDGenerator(this.projectInput)
-        await crud.init()
+        // Add CRUD for models
+        if (this.projectInput.generate.spa.crud) {
+          const crud = new BuefyCRUDGenerator(this.projectInput)
+          await crud.init()
+        }
       }
       console.log('Project Generated Successfully')
     } catch (e) {
