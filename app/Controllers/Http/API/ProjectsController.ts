7d3ba37920d7d05c9a83db98de8e6f8ea5d9a0ba
Ravindra <22148496+RSCipher001@users.noreply.github.com>
2021-09-26 22:38:47 +0530

    Pagination and download option added

--- app/Controllers/Http/API/ProjectsController.ts
+++ app/Controllers/Http/API/ProjectsController.ts
@@ -1,11 +1,18 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Generator from 'App/Services/ProjectService'
+import Env from '@ioc:Adonis/Core/Env'
 import Project from 'App/Models/Project'
+import Route from '@ioc:Adonis/Core/Route'
+import Generator from 'App/Services/ProjectService'
+import HelperService from 'App/Services/HelperService'
+import Application from '@ioc:Adonis/Core/Application'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CreateProjectValidator from 'App/Validators/CreateProjectValidator'
 
 export default class ProjectsController {
-  public async index({ auth }: HttpContextContract) {
-    return Project.query().where('userId', auth.user!.id)
+  public async index({ request, auth }: HttpContextContract) {
+    const page = request.input('page_no', 1)
+    const limit = request.input('page_size', 10)
+
+    return Project.query().where('userId', auth.user!.id).paginate(page, limit)
   }
 
   public async store({ request, auth }: HttpContextContract) {
@@ -19,6 +26,48 @@ export default class ProjectsController {
     return project
   }
 
+  public async generateSignedUrl({ request, auth }) {
+    const projectId = request.param('projectId')
+    const type = request.param('type')
+    const project = await Project.query()
+      .where({
+        userId: auth.user!.id,
+        id: projectId,
+      })
+      .firstOrFail()
+    if (!project) throw new Error('Project not found')
+    if (project.status === 'queued') throw new Error('Build is in progress')
+    if (project.status === 'failed') throw new Error('Build failed')
+    return Route.makeSignedUrl('projectDownload', {
+      projectId, type,
+    })
+  }
+
+  public async download({ request, response }: HttpContextContract) {
+    if (!request.hasValidSignature()) {
+      throw new Error('Signature is invalid');
+    }
+    const id = request.param('projectId')
+    const type = request.param('type')
+    const project = await Project.findOrFail(id)
+    const input = JSON.parse(project.rawInput)
+    const basePath = Application.makePath(Env.get('PROJECT_PATH'))
+    const names = HelperService.generateExtendedNames(input.name)
+    const apiPath = `${basePath}/${names.dashCase}`
+    // const uiPath = `${basePath}/${names.dashCase}-spa`
+
+    if (type === 'api') {
+      await HelperService.execute('git', ['archive', '--format', 'zip', '--output', 'api.zip', 'master'], {
+        cwd: apiPath
+      });
+      return response.download(`${apiPath}/api.zip`, true)
+    }
+    if (type === 'web') {
+      // Generate a zip for API folder
+      // Download it
+    }
+  }
+
   protected async generateProject(input, project: Project) {
     try {
       const generator = new Generator(input)
@@ -27,8 +76,16 @@ export default class ProjectsController {
       await project.save()
       // Send an email or something
     } catch (e) {
-      // Delete project files or something to undo the process
-      project.status = 'error'
+      const basePath = Application.makePath(Env.get('PROJECT_PATH'))
+      const names = HelperService.generateExtendedNames(input.name)
+      const apiPath = `${basePath}/${names.dashCase}`
+      const uiPath = `${basePath}/${names.dashCase}-spa`
+      try {
+        await HelperService.execute('rm', ['-f', apiPath, uiPath], {
+          cwd: basePath,
+        })
+      } catch (e) {}
+      project.status = 'failed'
       await project.save()
       console.error(e)
     }
--- start/routes.ts
+++ start/routes.ts
@@ -8,11 +8,19 @@ Route.group(() => {
   Route.post('/login', 'API/AuthController.login')
   Route.post('/register', 'API/AuthController.register')
   Route.post('/logout', 'API/AuthController.logout')
+
+  // Public API for downloading project
+  Route.get('/project/:projectId/download/:type', 'API/ProjectsController.download').as('projectDownload')
 }).prefix('/api')
 
 Route.group(() => {
   Route.get('/me', 'API/AuthController.me')
+
+  Route.get('/project', 'API/ProjectsController.index')
   Route.post('/project', 'API/ProjectsController.store')
+
+  // Project link generate and download options
+  Route.get('/project/:projectId/generate/:type', 'API/ProjectsController.generateSignedUrl').as('generateSignedUrl')
 })
   .middleware(['auth'])
   .prefix('/api')
