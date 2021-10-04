e248864a849097d20679b76e5fcdaf5500ab1fed
Ravindra <22148496+RSCipher001@users.noreply.github.com>
2021-10-05 00:50:17 +0530

    CI fixed

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,16 +1,7 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
 import Project from 'App/Models/Project'
-import ResetToken from 'App/Models/ResetToken'
-import {
-  column,
-  beforeSave,
-  BaseModel,
-  hasMany,
-  HasMany,
-  hasOne,
-  HasOne,
-} from '@ioc:Adonis/Lucid/Orm'
+import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
