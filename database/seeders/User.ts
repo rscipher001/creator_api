2bbc3e5c5eed56d69a7055490dbc0a5e58509433
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2022-05-17 23:49:06 +0530

    Random tests added

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,7 +1,21 @@
+import { DateTime } from 'luxon'
+import User from 'App/Models/User'
+import Env from '@ioc:Adonis/Core/Env'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-
 export default class UserSeeder extends BaseSeeder {
   public async run() {
-    // Write your database queries inside the run method
+    if (Env.get('NODE_ENV') === 'test') {
+      await User.firstOrCreate(
+        {
+          email: 'john@example.com',
+        },
+        {
+          name: 'John Doe',
+          email: 'john@example.com',
+          emailVerifiedAt: DateTime.now(),
+          password: 'john@example.com',
+        }
+      )
+    }
   }
 }
